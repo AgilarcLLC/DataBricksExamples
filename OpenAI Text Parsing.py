05b7f32fca9b0b2ccdea820944f37ae5f2bcb0c7
ChrisChris15 <ChrisChris15>
2023-05-17 18:31:31 +0000

    Added logic to access PDF directly from Databricks

--- OpenAI Text Parsing.py
+++ OpenAI Text Parsing.py
@@ -15,14 +15,55 @@ pip install openai
 
 # COMMAND ----------
 
+pip install pdfplumber
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ### We read directly from the PDF that was imported to Databricks
+# MAGIC
+# MAGIC File Location
+# MAGIC
+# MAGIC /Workspace/Users/chhirsh@gmail.com/Alimak-Scando-650-Brochure-w-max-lift-logo.pdf
+# MAGIC
+# MAGIC ### Script searches through entire PDF marking any pages that contain the words "Technical specifications"
+
+# COMMAND ----------
+
 import os
 import openai
+import pdfplumber
+import re
 import pandas as pd
 
-openai.api_key = "sk-uCplXmEuxxxxxxxxxxxxxxcU7hXqhgWBbLaBj"
+openai.api_key = "sk-uCplXmEuLWmxxxxxxxxxxxxxZqpcU7hXqhgWBbLaBj"
 results = []
 
-page_text = 'ALIMAK SCANDO 650\nMain characteristics\n• Robust design and excellent comfort \n• Modular car design with extensions and multiple\ndoor and ramp options\n• Single and dual car configurations \n• Triple entrance possible at ground level and at any\nlanding level\n• A high-efficiency helical gearbox provides lower\npower consumption and reduced operational costs\n• ALC-II collective control system with group control\nand internal fault diagnostics system. Control panel\nmovable between two sides \n• Built-in stainless steel electrical cabinet maximises\ninternal hoist dimensions for increased lifting\ncapacity \n• Wide range of optional equipment and functions\n• Remote Monitoring System — A3\n• Fully compliant with EN, ANSI, AS, PB and the\nmajority of all national regulations\nTECHNICAL SPECIFICATIONS\nALIMAK SCANDO 650 DOL FC\nMotor control Direct-on-line (DOL) Frequency control (FC)\nMax. payload capacity 1,500–3,200 kg (3,300–7,054 lbs) * 1,500–3,200 kg (3,300–7,054 lbs) *\nMax. travelling speed 38 m/min. (125 fpm) 66 m/min.(216 fpm)\nMax. lifting height 250 / 400 m (825 / 1300 ft) * 250 / 400 m (825 / 1,300 ft) *\nCar width, internal 1.5 m (4’-11”) 1.5 m (4’-11”)\nCar length, internal 2.8–5.0 m (9\'- 2 1⁄4"-16\'- 4 3⁄4") 2.8–5.0 m (9\'- 2 1⁄4"-16\'- 4 3⁄4")\nCar height, internal 2.3 m (7’-6 1/2”) 2.3 m (7’-6 1/2”)\nMax. load space per car 17 m3(610 ft3) 17 m3(610 ft3)\nNo. of motors 2 x 11 kW 2 or 3 x 11 kW\nSafety device type ALIMAK GFD  ALIMAK GFD\nPower supply range 400–500 V, 50 or 60 Hz, 3 phase  400–500 V, 50 or 60 Hz, 3 phase\nFuse ratings From 60 A From 63 A \nType of mast 650, tubular steel with integrated rack 650, tubular steel with integrated rack\nLength mast section 1.508 m (4’-11 3⁄8”) 1.508 m (4’-11 3⁄8”)\nWeight mast section with 1 rack 118 kg (260 lbs) 118 kg (260 lbs)\nRack module 5  5 \n* Increased payload capacity and lifting height and on request.\nFor other demands or specifications, please consult your Alimak representative.\n9\n1\n0\nwww.alimak.com 2\nb. \ne\nF\nN/\nE\n7 \nPictures are illustrative only and do not necessarily show the configuration of products on the market at a given point in time. Products must be used in conformity with safe practice and applicable 21\nstatutes, regulations, codes and ordinances. Specifications of products and equipment shown herein are subject to change without notice. Copyright © 2019 Alimak Group. All rights reserved.  1\nAlimak and Scando are registered trademarks of Alimak Group. '
+#Open File an search for "Technical specifications"
+
+pdf_file = "/Workspace/Users/chhirsh@gmail.com/Alimak-Scando-650-Brochure-w-max-lift-logo.pdf"
+pattern = re.compile(r"Technical specifications", flags=re.IGNORECASE)
+with pdfplumber.open(pdf_file) as pdf:
+    pages = pdf.pages
+    for page_nr, pg in enumerate(pages, 0):
+        content = pg.extract_text()
+        for match in pattern.finditer(content):
+            print('Keyword:',match.group(), '|  Page:', page_nr, '| Location: ',content.index(match.group()))
+            foundPageNum = page_nr
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ### Show the Text extracted from the PDF
+
+# COMMAND ----------
+
+table_page = pdf.pages[foundPageNum]
+testouput01 = table_page.extract_text_simple(x_tolerance=3, y_tolerance=3)
+
+testouput01
 
 # COMMAND ----------
 
